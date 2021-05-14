1b706c390e28fa042cefcc431ab62226588357e4
keshavrathi007 <45044122+keshavrathi007@users.noreply.github.com>
2021-05-14 16:53:41 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,6 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBZbt0TR796CFjgxDfNQ8zLtmgDV6Lfb48",
-    authDomain: "messenger-clone-2c3ba.firebaseapp.com",
-    projectId: "messenger-clone-2c3ba",
-    storageBucket: "messenger-clone-2c3ba.appspot.com",
-    messagingSenderId: "900035534621",
-    appId: "1:900035534621:web:1cd1811372c580dc294bf4"
 });
 
 const db = firebaseApp.firestore();
